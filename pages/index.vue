3dbc082af8192804b8f6ba133e4c5cd85b9ba66e
ballveerachi <mju6204101372@mju.ac.th>
2022-10-05 14:29:50 +0700

    frist commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <FormComponent />
+  <FormPlanCareer/>
+  <FormQualification/>
+  <FormPlan/>
+  <FormSelfAssessment/>
+  <FormPivotTable />
+  <h1>222222</h1>
+  
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'IndexPage',
+
+}
+</script>
